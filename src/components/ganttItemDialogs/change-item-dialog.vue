fb88949339f36d3e4cd38a922f23fd6c5367f7c3
Jtomat <stragnik13@gmail.com>
2021-10-16 09:33:23 +0500

    add dialogs

--- src/components/ganttItemDialogs/change-item-dialog.vue
+++ src/components/ganttItemDialogs/change-item-dialog.vue
@@ -5,9 +5,20 @@
     @close="closeDialog"
     @open="openDialog"
   >
-    <p :style="{ margin: '25px', textAlign: 'center' }">
-      ИЗМЕНИТЬ
-    </p>
+    <div :style="{ margin: '25px', textAlign: 'center' }">
+      <div class="m-3">
+        <p>Название задачи</p>
+        <k-input></k-input>
+      </div>
+      <div class="m-3">
+        <p>Начало выполнения</p>
+        <p><k-input type="time" /><k-input type="date" /></p>
+      </div>
+      <div class="m-3">
+        <p>Конец выполнения</p>
+        <p><k-input type="time" /><k-input type="date" class="m-2" /></p>
+      </div>
+    </div>
     <dialog-actions-bar>
       <button class="k-button" @click="closeDialog">Отменить</button>
       <button class="k-button" @click="saveChanges">Сохранить изменения</button>
@@ -17,7 +28,7 @@
 
 <script>
 import { Dialog, DialogActionsBar } from "@progress/kendo-vue-dialogs";
-
+import { Input } from "@progress/kendo-vue-inputs";
 export default {
   name: "change-item-dialog",
   components: {
@@ -40,7 +51,7 @@ export default {
       this.visibleDialog = true;
     },
     saveChanges() {
-      console.log('Save!')
+      console.log("Save!");
       this.closeDialog();
     },
   },
--- src/components/ganttItemDialogs/create-item-dialog.vue
+++ src/components/ganttItemDialogs/create-item-dialog.vue
@@ -1,13 +1,25 @@
 <template>
   <k-dialog
     v-if="visibleDialog"
+    :width="500"
     :title="'Добавить элемент'"
     @close="closeDialog"
     @open="openDialog"
   >
-    <p :style="{ margin: '25px', textAlign: 'center' }">
-      ДОБАВИТЬ
-    </p>
+    <div :style="{ margin: '25px', textAlign: 'center' }">
+      <div class="m-3">
+        <p>Название задачи</p>
+        <k-input></k-input>
+      </div>
+      <div class="m-3">
+        <p>Начало выполнения</p>
+        <p><k-input type="time" /><k-input type="date" /></p>
+      </div>
+      <div class="m-3">
+        <p>Конец выполнения</p>
+        <p><k-input type="time" /><k-input type="date" class="m-2" /></p>
+      </div>
+    </div>
     <dialog-actions-bar>
       <button class="k-button" @click="closeDialog">Отменить</button>
       <button class="k-button" @click="createItem">Добавить</button>
@@ -18,17 +30,18 @@
 <script>
 import { defineComponent } from "vue";
 import { Dialog, DialogActionsBar } from "@progress/kendo-vue-dialogs";
-
+import { Input } from "@progress/kendo-vue-inputs";
 export default defineComponent({
   name: "create-item-dialog",
   components: {
     "k-dialog": Dialog,
     "dialog-actions-bar": DialogActionsBar,
+    "k-input": Input,
   },
   data() {
     return {
-      visibleDialog: false
-    }
+      visibleDialog: false,
+    };
   },
   mounted() {
     this.visibleDialog = this.visible;
