ec1679b23ab9a56b365b26e6ecd5837db5ad52fc
Jtomat <stragnik13@gmail.com>
2021-11-13 09:36:28 +0500

    heroku create

--- src/http-clients/worker-role-client.ts
+++ src/http-clients/worker-role-client.ts
@@ -1,5 +1,26 @@
+import { $host } from "@/http-clients/clients";
+
 export interface WorkerRoleModel {
   id: number;
   name: string;
   rate: number;
+}
+
+export class WorkerRoleClient {
+  _api = "/au/conf/roles/";
+
+  async updateRole(role: WorkerRoleModel): Promise<any> {
+    const { data } = await $host.put<any>(this._api + `${role.id}`, role);
+    return data;
+  }
+
+  async addRole(role: WorkerRoleModel): Promise<any> {
+    const { data } = await $host.post<any>(this._api, role);
+    return data;
+  }
+
+  async delete(role: WorkerRoleModel): Promise<any> {
+    const { data } = await $host.delete<any>(this._api + `${role.id}`);
+    return data;
+  }
 }
